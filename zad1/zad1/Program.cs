b50c1eb65bc84ceddd74ab178cf7f25714fc142a
YanaSkorb <skorbyaras29@gmail.com>
2022-06-21 11:31:56 +0300

    Initial commit

---
+++ zad1/zad1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace zad1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<char> str = new Stack<char>();
+            Console.Write("Введите строку: ");
+            string s = Console.ReadLine();
+            for (int i = 0; i < s.Length; i++)
+            {
+                str.Push(s[i]);
+            }
+            s = "";
+            char ch;
+            int count = str.Count;
+            for (int i = 0; i < count; i++)
+            {
+                ch = str.Pop();
+                if (ch == '#')
+                {
+                    str.Pop();
+                    i++;
+                }
+                else
+                {
+                    s += ch;
+                }
+            }
+            Console.WriteLine("Результат работы : {0}", s);
+            Console.ReadLine();
+
+        }
+    }
+}
+
---
+++ zad3/Sport.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace zad3
+{
+    public class Sport
+    {
+        public string s;
+        public string g;
+        public Sport(string s, string g)
+        {
+            this.s = s;
+            this.g = g;
+        }
+        virtual public void PrintSport()
+        {
+            Console.WriteLine("В "+g+" состоится игра по "+ s);
+        }       
+    }
+}
